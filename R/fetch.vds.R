5f9fb3dd0918016a3783eedeccc0f7d4ce581eb1
James E. Marca <jmarca@translab.its.uci.edu>
2014-09-26 21:01:25 -0700

    sequence of maps created as pdf files

--- R/fetch.vds.R
+++ R/fetch.vds.R
@@ -7,10 +7,3 @@ library(testthat)
 vds.query <- "select v.* from vds_current_view v join carb_airdistricts_aligned_03 c on (ST_Contains(c.geom4326, v.geom)) where c.name ~* 'joaquin' and vdstype='ML'"
 
 vds.df <- read.ogr::readOgrSQL(vds.query)
-
-wim.query <- "select w.* from wim_geoview w join carb_airdistricts_aligned_03 c on (ST_Contains(c.geom4326, w.geom)) where c.name ~* 'joaquin' and  wim_type ~* 'data'"
-
-wim.df <- read.ogr::readOgrSQL(wim.query)
-
-
-expect_that(dim(wim.df),equals(c(28,9)))
--- R/process.vds.files.R
+++ R/process.vds.files.R
@@ -1,3 +1,4 @@
+library(sp)
 
 wim.km <- spDists(wim.df,longlat=TRUE)
 
@@ -8,20 +9,18 @@ wim.vds.km <- spDists(wim.df,vds.df,longlat=TRUE)
 dim(vds.df)
 
 limit <- 16 ## 500 km is too long
-km.limited <- km
 
-close.idx <- km.limited<limit
-km.limited[!close.idx] <- limit+1
+
+
+covered <-list()
 
 ## first assign locations to the WIM stations
+## use the maximum cover first, then move down the list
 
-
-
-covered.idx <- rep(FALSE,length(ranking))
-covering.set <- c()
-covered <-list()
-## start the loop here
-while( sum(!covered.idx) > 0 ){
+wim.covering.set <- c()
+close.idx <- wim.vds.km<limit
+covered.idx <- rep(FALSE,dim(vds.df)[1])
+while( sum(close.idx[,!covered.idx]) > 0  ){
     if(sum(!covered.idx)==1){
         convert.site <- which.min(covered.idx)
     }else{
@@ -31,51 +30,101 @@ while( sum(!covered.idx) > 0 ){
     new.sites.idx <- close.idx[convert.site,]
     ## drop ones already covered
     new.sites.idx[covered.idx] <- FALSE
-    ##    covered[[length(covering.set)+1]] <- list(cvd.idx=new.sites.idx,cvd.site=convert.site)
-    df[new.sites.idx,'group'] <- df@data$id[convert.site]
+    vds.df@data$group[new.sites.idx] <- paste(c('wim',wim.df@data[convert.site,c('site_no','direction')]),collapse='.')
     covered.idx <- covered.idx | close.idx[convert.site,]
-    covering.set <- c(covering.set,convert.site)
+    wim.covering.set <- c(wim.covering.set,convert.site)
 }
 
-covering.set
-
-df[covering.set,'group'] <- 'source'
+wim.covered.idx <-  covered.idx
 
 
-##df@data$rid = rownames(df@data)
-##vds.points <- fortify(df,region="rid")
-##vds.df <- join(vds.points,df@data,by="rid")
+## reset things
+cvds.covering.set <- c()
+close.idx <- vds.km<limit  ## now looking at vds to vds distances
+## keep covered.idx the same to avoid double counting
 
-
-sjvmap <- ggplot2::ggplot(df@data, aes(x=longitude, y=latitude, group=freeway_id)) +
-    geom_point()+ coord_equal()
-
-sjvmap
+while(
+    sum(close.idx[,!covered.idx])
+    > 0
+){
+    if(sum(!covered.idx)==1){
+        convert.site <- which.min(covered.idx)
+    }else{
+        ranking <- rowSums(close.idx[,!covered.idx])
+        convert.site <- which.max(ranking)
+    }
+    new.sites.idx <- close.idx[convert.site,]
+    ## drop ones already covered
+    new.sites.idx[covered.idx] <- FALSE
+    vds.df[new.sites.idx,'group'] <- vds.df@data$id[convert.site]
+    covered.idx <- covered.idx | close.idx[convert.site,]
+    cvds.covering.set <- c(cvds.covering.set,convert.site)
+}
+vds.covered.idx <- covered.idx
+vds.covered.idx[wim.covered.idx] <- FALSE
 
 library(OpenStreetMap)
 
 upperleft <-  c(max(df@data$latitude)+0.5,min(df@data$longitude)-0.1)
 lowerright <- c(min(df@data$latitude)-0.5,max(df@data$longitude)+0.1)
 
-#mp <- OpenStreetMap::openmap(upperleft,lowerright,7, 'stamen-watercolor' )
+
 #mp <- OpenStreetMap::openmap(upperleft,lowerright,7, 'stamen-toner' )
 ## 11 crashies RAM
 mpt <- OpenStreetMap::openmap(upperleft,lowerright,9, 'stamen-terrain' )
 
-pdf("map_sites.pdf", width = 8, height = 10)
+mpw <- OpenStreetMap::openmap(upperleft,lowerright,9, 'stamen-watercolor' )
+
 
 map_longlat <- openproj(mpt)
 
-b <- ggplot2::autoplot(map_longlat)
+mapw_longlat <- openproj(mpw)
 
-b <- b +
-    geom_point(data=df@data, aes(x=longitude, y=latitude, color=factor(group)))
 
-b <- b +
-    geom_point(data=df@data[df@data$group=='source',], aes(x=longitude, y=latitude, size=2))
+areamap <- ggplot2::autoplot(map_longlat)
 
-b <- b + coord_equal()
 
-b
+c <-  geom_point(data=wim.df@data,
+                 aes(x=longitude, y=latitude), colour="grey50", alpha=0.5,size = 2)
+d <-
+    geom_point(data=wim.df@data,
+               aes(x=longitude, y=latitude), fill = "dark grey", colour = "black", alpha = 1/3,size = 3)
+
+
+cvds <- geom_point(data=vds.df@data[cvds.covering.set,],
+                aes(x=longitude, y=latitude),
+                fill = "grey", colour = "blue", alpha = 1/3,size = 3)
+
+cwim <- geom_point(data=wim.df@data[wim.covering.set,],
+                aes(x=longitude, y=latitude),
+                fill = "grey", colour = "red", alpha = 1/3,size = 3)
+
+
+vdsassignedtowim <- geom_point(data=vds.df@data[wim.covered.idx,], aes(x=longitude, y=latitude, color=factor(group)))
+
+
+vdsassigned <- geom_point(data=vds.df@data, aes(x=longitude, y=latitude, color=factor(group)))
+
+
+
+vdsonly <- geom_point(data=vds.df@data, aes(x=longitude, y=latitude))
+
+
+pdf("map_sites.pdf", width = 8, height = 10)
+
+areamap
+
+areamap + c + d
+
+areamap + vdsonly
+
+areamap + c + d + vdsonly
+
+areamap+c+d+vdsonly+cwim+vdsassignedtowim
+
+areamap+c+d+vdsonly+cwim+vdsassigned
+
+areamap + c + d + cwim
+
 
 dev.off()
